76526b714e7a58bd80016afaaa790852054b8cd2
Kimberly Gao <52011157+kimberlygao@users.noreply.github.com>
2022-12-25 17:06:54 -0600

    work page setup

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -9,45 +9,33 @@ async function populate() {
 
   let div = $(".projects");
   let offset = 3;
-  for(let i = 0; i < 6; i++) {
+  for(let i = 0; i < projects.length; i++) {
     
     console.log(projects[i])
-    // var $card  = $("<div>", {"class": "card", "text": projects[i]["name"]})
-    // var $cardBody = $("<div", {"class": "card-body text-center"})
-    // $("<h6>", {"class": "card-title", "text": projects[i]["name"]}).appendTo($cardBody)
-    var cardDiv = $('<div></div>').addClass("col-lg-3 d-flex justify-content-center")
+    var cardDiv = $('<div></div>').addClass("col p-4 mb-5")
     
-    var card = $('<div></div>').addClass("card project-card mt-5 mb-5")
-    var cardBody = $('<div></div>').addClass("card-body").append($("<h6></h6>").addClass("card-title").text(projects[i]["name"]))
-    var tags = $('<p></p>').addClass("card-subtitle")
-    var tagList = projects[i]['tags']
-    for(let j = 0; j < tagList.length; j++) {
-      tags.append($('<span></span>').text(tagList[j]).addClass("card-tag"))
-    } 
-    if (projects[i]["url"] != "") {
-      let anchor = $('<a></a>').attr("href", projects[i]["url"]).attr("target","_blank").attr("rel", "noopener noreferrer")
-      anchor.append($("<i></i>").addClass("card-link bi bi-box-arrow-up-right text-right pl-1"))
-      tags.append(anchor)
-    }
-      
-    
+    var card = $('<div></div>').addClass("card")
+    card.append($('<img/>').attr('class', "card-img-top").attr('src', projects[i]["img"]))
+
+
+    var cardBody = $('<div></div>').addClass(`card-body ${projects[i]["color"]}-bg`).append($("<h4></h4>").addClass("card-title").text(projects[i]["name"]))
+    var description = $('<p></p>').addClass("card-text").text(projects[i]["description"])
+    var tags = $('<p></p>').addClass("card-tag").text(projects[i]["tags"])
+
+    cardBody.append(description)
     cardBody.append(tags)
 
-   
-    card.append($('<img/>').attr('class', "card-img-top").attr('src', projects[i]["img"]))
+    // if (projects[i]["url"] != "") {
+    //   let anchor = $('<a></a>').attr("href", projects[i]["url"]).attr("target","_blank").attr("rel", "noopener noreferrer")
+    //   anchor.append($("<i></i>").addClass("card-link bi bi-box-arrow-up-right text-right pl-1"))
+    //   tags.append(anchor)
+    // }
+      
+    
     card.append(cardBody)
+
     cardDiv.append(card)
-
-    if (offset == 0) {
-      cardDiv.addClass("offset-lg-2")
-      // let newRow = $("<div></div>").addClass("row")
-      // div.append(newRow)
-      // div = newRow
-      offset = 3;
-    }
-
     div.append(cardDiv)
-    offset--;
   }
 
 }
